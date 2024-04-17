0a29b9db11c23e3dc23e0e2a129dbfff4eb04e8d
Maxim <maximgratii@gmail.com>
2024-04-17 12:18:33 +0300

    fix

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,29 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  id: string;
+  lang?: string;
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule'
+    name: '@feedefy/nuxt',
+    configKey: 'feedefy',
+    compatibility: {
+      nuxt: '^3.0.0'
+    }
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
+  defaults: {
+    id: '',
+    lang: ''
+  },
   setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const resolver = createResolver(import.meta.url);
 
+    nuxt.options.runtimeConfig.feedefyOptions = options;
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    addPlugin({ src: resolver.resolve('./runtime/plugin'), mode: 'client' }, )
   }
 })
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,30 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log('Plugin injected by my-module!')
+  const { id, lang } = useRuntimeConfig().feedefyOptions as any;
+
+  nuxtApp.hook('app:mounted', () => {
+    const existingScript = document.querySelector(`script[src*="https://app.feedefy.com"]`);
+
+    if (existingScript) {
+      return;
+    }
+
+    if (!id) {
+      console.error("No projectId was passed to Feedefy Nuxt, widget will not be initialized");
+      return;
+    }
+
+    const script = document.createElement("script");
+
+    if (lang) {
+      script.setAttribute("lang", lang);
+    }
+
+    script.setAttribute("src", `https://app.feedefy.com/embed.js?id=${id}`);
+    script.defer = true;
+    script.addEventListener("error", () => script.remove());
+
+    document.body.appendChild(script);
+  })
 })
